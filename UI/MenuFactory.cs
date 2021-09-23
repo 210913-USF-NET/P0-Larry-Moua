8911a38ab1064596a31b7593887ba533d71d3c14
unknown <lkmoua@gmail.com>
2021-09-23 14:21:43 -0400

    added dbcontext to menufactory

--- UI/MenuFactory.cs
+++ UI/MenuFactory.cs
@@ -1,5 +1,8 @@
 using DL;
 using RBBL;
+using DL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
 
 namespace UI
 {
@@ -7,6 +10,12 @@ namespace UI
     {
         public static IMenu GetMenu(string menuString)
         {
+            string connectionString = File.ReadAllText(@"../connectionString.txt");
+
+            DbContextOptions<kpopsnapshotdbContext> options = new DbContextOptionsBuilder<kpopsnapshotdbContext>().UseSqlServer(connectionString).Options;
+            
+            kpopsnapshotdbContext context = new kpopsnapshotdbContext(options);
+
             switch (menuString.ToLower())
             {
                 case "main":
